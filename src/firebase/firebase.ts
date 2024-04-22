d3178b554e117b5e19a4f2543caf0686b7bbf566
KAMADO <abdonsila2004@gmail.com>
2024-04-22 19:23:29 +0100

    Add Firebase configuration and authentication setup

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBXBh7BHxCbhNRE_qd1OnL65mHZicR2ZA",
+  authDomain: "exop-d02fc.firebaseapp.com",
+  projectId: "exop-d02fc",
+  storageBucket: "exop-d02fc.appspot.com",
+  messagingSenderId: "253073675184",
+  appId: "1:253073675184:web:273310574e3b5bc0e07c75"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+
