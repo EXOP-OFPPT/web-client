0cc5703523e8af0ae35b4a5fc5719be2946a1d6a
KAMADO <abdonsila2004@gmail.com>
2024-04-24 21:56:08 +0100

    Refactor RootLayout component to update UI and add logout functionality

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,38 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { logoutUser } from "@/state/auth/AuthSlice";
+import { AppDispatch } from "@/state/store";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+// --------------------------------------------------------
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
 
-const RootLayout: React.FC = () => {
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+    setTimeout(() => {
+      navigate("/login");
+    }, 500);
+  };
 
   return (
-    <div className="min-h-[100vh] overflow-hidden">
-      <Outlet />
+    <div className="min-h-[100vh] w-full overflow-hidden">
+      <div className="absolute right-8 top-4">
+        <ModeToggle />
+      </div>
+      <Button
+        className="bg-red-500 hover:bg-red-600 text-white mt-4 ml-8"
+        onClick={handleLogout}
+      >
+        Logout
+      </Button>
+      {children}
     </div>
   );
 };
