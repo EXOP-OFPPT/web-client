6e77ba41c9a28340c0a157399b57065b9b346d63
KAMADO <abdonsila2004@gmail.com>
2024-05-10 20:21:45 +0100

    refactor: Add deleteKpiSlice for managing KPI deletion

---
+++ src/state/Kpis/DeleteSlice.ts
@@ -0,0 +1,89 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AppThunk } from "../store";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "@/firebase/firebase";
+import { getKpis } from "./GetSlice";
+
+// Interface for error
+interface Error {
+  code: string;
+  message: string;
+}
+
+export type KpiType = {
+  code: string;
+  title: string;
+  description: string;
+  value: string;
+};
+
+// Interface for DeleteState
+interface DeleteState {
+  loading: boolean;
+  error: Error | null;
+  message: string | null;
+}
+
+// Initial state
+const initialState: DeleteState = {
+  loading: false,
+  error: null,
+  message: null,
+};
+
+// Create slice
+const deleteKpiSlice = createSlice({
+  name: "deleteKpiSlice",
+  initialState,
+  reducers: {
+    actionSuccess: (state, action: PayloadAction<string | null>) => {
+      state.loading = false;
+      state.message = action.payload;
+    },
+    actionFailed: (state, action: PayloadAction<Error | null>) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.loading = action.payload;
+    },
+    setMessage: (state, action: PayloadAction<string | null>) => {
+      state.message = action.payload;
+    },
+    setError: (state, action: PayloadAction<Error | null>) => {
+      state.error = action.payload;
+    },
+    clearMessageAndError: (state) => {
+      state.message = null;
+      state.error = null;
+    },
+  },
+});
+
+export const {
+  actionSuccess,
+  actionFailed,
+  setLoading,
+  setMessage,
+  setError,
+  clearMessageAndError,
+} = deleteKpiSlice.actions;
+
+export default deleteKpiSlice.reducer;
+
+// Thunk to delete employee
+export const deleteKpi =
+  (docId: string): AppThunk =>
+    async (dispatch) => {
+      // Reset message and error
+      dispatch(setLoading(true));
+      dispatch(clearMessageAndError());
+      try {
+        // Delete employee document
+        await deleteDoc(doc(db, "kpi", docId));
+        dispatch(actionSuccess("Kpi deleted successfully"));
+        dispatch(getKpis());
+      } catch (error: any) {
+        dispatch(actionFailed({ code: error.code, message: error.message }));
+      }
+    };
