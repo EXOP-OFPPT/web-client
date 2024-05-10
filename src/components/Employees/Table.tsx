cdc178bbefe294c956fda135f7a21029ab6410e5
KAMADO <abdonsila2004@gmail.com>
2024-05-10 20:19:47 +0100

    refactor: Filter out current user from displayed employees in EmployeesTable component

--- src/components/Employees/Table.tsx
+++ src/components/Employees/Table.tsx
@@ -2,13 +2,18 @@ import { useSelector } from "react-redux";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
 import { RootState } from "@/state/store";
+import Cookies from "universal-cookie";
+const cookies = new Cookies(null, { path: "/" });
 
 export default function EmployeesTable() {
+  const user = cookies.get("user");
   const data = useSelector((state: RootState) => state.getEmployees.employees);
+
+
 
   return (
     <div className="w-full xlg:w-4/5 md:w-11/12">
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={data.filter(item => item.email !== user.email)} />
     </div>
   );
 }
