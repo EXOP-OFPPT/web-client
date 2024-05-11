40c59894a9ade86ac0d341ea9182cf70d71e92d9
KAMADO <abdonsila2004@gmail.com>
2024-05-11 22:36:31 +0100

    chore: Add lazy loading for images in RootLayout component

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,23 +1,40 @@
 import NavBar from "@/components/Dashboard/Navbar";
 import Settings from "@/components/Dashboard/Settings";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { RootState } from "@/state/store";
+import { AppDispatch, RootState } from "@/state/store";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useToast } from "@/components/ui/use-toast";
 import { Toaster } from "@/components/ui/toaster";
 import { CheckCircle2 } from "lucide-react";
 import { clearMessageAndError } from "@/state/auth/AuthSlice";
+import { getEmployees } from "@/state/Employees/GetSlice";
+import { getTasks } from "@/state/Tasks/GetSlice";
+import Cookies from "universal-cookie";
+const cookies = new Cookies(null, { path: "/" });
 
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+  const user = cookies.get("user");
+  const employees = useSelector((state: RootState) => state.getEmployees.employees);
+  const tasks = useSelector((state: RootState) => state.getTasks.tasks);
   const isLogin = useSelector((state: RootState) => state.auth.isLogin);
   const message = useSelector((state: RootState) => state.auth.message);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
   const { toast } = useToast();
+
+  useEffect(() => {
+    if (employees.length === 0) {
+      dispatch(getEmployees());
+    }
+    if (tasks.length === 0) {
+      dispatch(getTasks(user.role, user.email));
+    }
+
+  }, [dispatch]);
 
   useEffect(() => {
     if (isLogin && message) {
