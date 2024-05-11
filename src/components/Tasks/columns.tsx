08c6ffa263a0d53aef869b3b7b3d8196f2d29e1e
KAMADO <abdonsila2004@gmail.com>
2024-05-11 22:34:54 +0100

    Refactor Profile/States component to improve task filtering and productivity calculation

---
+++ src/components/Tasks/columns.tsx
@@ -0,0 +1,275 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { ColumnDef } from "@tanstack/react-table";
+import { ChevronsUpDown } from "lucide-react";
+import Delete from "./Delete";
+import Update from "./Update";
+import Verify from "./Verify";
+import { TaskType } from "@/state/Tasks/GetSlice";
+import { Card } from "../ui/card";
+import { Badge } from "../ui/badge";
+import { Timestamp } from "firebase/firestore";
+import Cookies from "universal-cookie";
+const cookie = new Cookies(null, { path: "/" });
+
+
+export const columns: ColumnDef<TaskType>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => {
+          table.toggleAllPageRowsSelected(!!value);
+        }}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  // {
+  //   id: "id",
+  //   accessorKey: "id",
+  //   header: ({ column }) => {
+  //     return (
+  //       <Button
+  //         variant="ghost"
+  //         onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+  //       >
+  //         Id
+  //         <ChevronsUpDown size={12} className="ml-2" />
+  //       </Button>
+  //     );
+  //   },
+  // },
+  {
+    id: "title",
+    accessorKey: "title",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Title
+          <ChevronsUpDown size={12} className="ml-2" />
+        </Button>
+      );
+    },
+  },
+  {
+    id: "description",
+    accessorKey: "description",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Description
+          <ChevronsUpDown size={12} className="ml-2" />
+        </Button>
+      );
+    },
+  },
+  {
+    id: "status",
+    accessorKey: "status",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Status
+          <ChevronsUpDown size={12} className="ml-2" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      let bgColor;
+      if (row.original.status === 'todo') {
+        bgColor = 'bg-neutral-400';
+      } else if (row.original.status === 'inprogress') {
+        bgColor = 'bg-yellow-500';
+      } else if (row.original.status === 'done') {
+        bgColor = 'bg-primary';
+      }
+      return (
+        <Badge variant="outline" className={`w-20 font-bold flex justify-center items-center py-2 px-4 ${bgColor} hover:${bgColor}`}>
+          {row.original.status.charAt(0).toUpperCase() + row.original.status.slice(1)}
+        </Badge>
+      )
+    }
+  },
+  {
+    id: "createdAt",
+    accessorKey: "createdAt",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Created At
+          <ChevronsUpDown size={12} className="ml-2" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      let createdAt;
+      if (row.original.createdAt instanceof Date) {
+        createdAt = row.original.createdAt;
+      } else if (typeof row.original.createdAt === 'string') {
+        createdAt = new Date(row.original.createdAt);
+      } else {
+        createdAt = new Date();
+      }
+
+      const date = createdAt.toLocaleDateString();
+
+      return <span>{date}</span>;
+    },
+  },
+  {
+    id: "deadLine",
+    accessorKey: "deadLine",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Dead Line
+          <ChevronsUpDown size={12} className="ml-2" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      let createdAt;
+      if (row.original.createdAt instanceof Timestamp) {
+        createdAt = row.original.createdAt.toDate();
+      } else if (typeof row.original.createdAt === 'string') {
+        createdAt = new Date(row.original.createdAt);
+      } else {
+        createdAt = new Date();
+      }
+
+      let deadline;
+      if (row.original.deadLine instanceof Timestamp) {
+        deadline = row.original.deadLine.toDate();
+      } else if (typeof row.original.deadLine === 'string') {
+        deadline = new Date(row.original.deadLine);
+      } else {
+        deadline = new Date();
+      }
+
+      const today = new Date();
+
+      // Ensure the dates are valid
+      if (isNaN(createdAt.getTime()) || isNaN(deadline.getTime())) {
+        console.error('Invalid date values');
+        return null;
+      }
+
+      const totalDuration = deadline.getTime() - createdAt.getTime();
+      const elapsedDuration = today.getTime() - createdAt.getTime();
+
+      // Ensure the total duration is not zero
+      if (totalDuration === 0) {
+        console.error('Total duration is zero');
+        return null;
+      }
+
+      const progress = Math.min(100, (elapsedDuration / totalDuration) * 100);
+      const date = deadline.toLocaleDateString();
+
+      return (
+        <Card className="border-neutral-700">
+          <div className="text-center bg-neutral-4000 w-full h-3 p-[1.5px]">
+            <div className={`!bg-blue-500 h-full flex justify-center items-center rounded-full`} style={{ width: `${progress}%` }}>
+            </div>
+            <span className="text-[13px]">{date}</span>
+          </div>
+        </Card>
+      );
+    },
+  },
+  {
+    id: "assignedTo",
+    accessorKey: "assignedTo",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Assigned To
+          <ChevronsUpDown size={12} className="ml-2" />
+        </Button>
+      );
+    },
+  },
+  {
+    id: "toverify",
+    accessorKey: "toverify",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          To Verify
+          <ChevronsUpDown size={12} className="ml-2" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return (
+        <>
+          <Verify info={row.original} />
+        </>
+      );
+    }
+  },
+  {
+    id: "action",
+    accessorKey: "action",
+    header: "Action",
+    cell: ({ row }) => {
+      return (
+        <div className="flex justify-center items-center gap-2">
+          <Update mode="outline" info={row.original} />
+          {(cookie.get("user").role == "admin") && <Delete mode="outline" docId={row.original.id} />}
+        </div>
+      );
+    }
+  },
+  // {
+  //   id: "kpiCode",
+  //   accessorKey: "kpiCode",
+  //   header: ({ column }) => {
+  //     return (
+  //       <Button
+  //         variant="ghost"
+  //         onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+  //       >
+  //         Kpi Code
+  //         <ChevronsUpDown size={12} className="ml-2" />
+  //       </Button>
+  //     );
+  //   },
+  // },
+
+];
