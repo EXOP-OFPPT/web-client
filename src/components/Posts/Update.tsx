e9f2758251f42f6f494dbf43ae8e6234e5e26203
KAMADO <abdonsila2004@gmail.com>
2024-05-15 23:08:51 +0100

    refactor: Update KPIs columns to include progress bar

---
+++ src/components/Posts/Update.tsx
@@ -0,0 +1,271 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+// import {
+//   Select,
+//   SelectContent,
+//   SelectItem,
+//   SelectTrigger,
+//   SelectValue,
+// } from "@/components/ui/select";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { AppDispatch, RootState } from "@/state/store";
+import { Loader2, CheckCircle2, CircleX, Edit } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { ScrollArea } from "../ui/scroll-area";
+import { useToast } from "../ui/use-toast";
+import { useEffect } from "react";
+import { Toaster } from "../ui/toaster";
+import {
+  clearMessageAndError,
+  updateKpi
+} from "@/state/Kpis/UpdateSlice";
+
+const formSchema = z.object({
+  title: z.string().min(2, {
+    message: "Title must be at least 2 characters.",
+  }),
+  description: z.string().min(2, {
+    message: "Description must be at least 2 characters.",
+  }),
+  minTaux: z.number(),
+  currentTaux: z.string({
+    required_error: "Current Taux is required",
+  }),
+  type: z.string(),
+});
+
+type infoProps = {
+  code: string;
+  title: string;
+  description: string;
+  type: string;
+  minTaux: number;
+  currentTaux: number;
+};
+
+type UpdateProps = {
+  mode: "ghost" | "outline";
+  info: infoProps;
+};
+
+const Update = ({ mode, info }: UpdateProps) => {
+  const isLoading = useSelector(
+    (state: RootState) => state.updateKpi.loading
+  );
+  const message = useSelector(
+    (state: RootState) => state.updateKpi.message
+  );
+  const error = useSelector((state: RootState) => state.updateKpi.error);
+  const dispatch = useDispatch<AppDispatch>();
+  const { toast } = useToast();
+
+  useEffect(() => {
+    if (message) {
+      toast({
+        variant: "default",
+        title: "Action dispatched",
+        description: message,
+        className: "text-primary border-2 border-primary text-start",
+        icon: <CheckCircle2 size={40} className="mr-2" />,
+      });
+      dispatch(clearMessageAndError());
+    } else if (error) {
+      toast({
+        variant: "default",
+        title: "Action Failed",
+        description: error?.message,
+        className: "text-error border-2 border-error text-start",
+        icon: <CircleX size={40} className="mr-2" />,
+      });
+      dispatch(clearMessageAndError());
+    }
+  }, [message, error]);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: info.title,
+      description: info.description,
+      minTaux: info.minTaux,
+      currentTaux: info.currentTaux.toString(),
+      type: info.type,
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    const data = {
+      code: info.code,
+      title: values.title,
+      description: values.description,
+      minTaux: values.minTaux,
+      currentTaux: parseInt(values.currentTaux),
+      type: values.type,
+    };
+    dispatch(updateKpi({ code: info.code, updatedData: data }));
+  }
+
+  return (
+    <>
+      <Toaster />
+      <Dialog>
+        <DialogTrigger className="w-full cursor-pointer" asChild>
+          <Button
+            className="h-8 w-8 hover:text-yellow-500"
+            variant={mode}
+            size="icon"
+          >
+            <Edit size={16} />
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="h-[650px] sm:max-w-[700px] px-1">
+          <ScrollArea className="h-full w-full p-4">
+            <DialogHeader className="mb-4">
+              <DialogTitle className="text-primary text-2xl">
+                Update Kpi
+              </DialogTitle>
+              <DialogDescription>
+                Fill in the form below to update kpi.
+              </DialogDescription>
+            </DialogHeader>
+            {/* <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4"> */}
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8 px-2 my-2"
+              >
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Title</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Title" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Description" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="minTaux"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Min Taux</FormLabel>
+                      <FormControl>
+                        <Input disabled placeholder="Min Taux" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="currentTaux"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Current Taux</FormLabel>
+                      <FormControl>
+                        <Input type="number" min={0} max={100} placeholder="Current Taux" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="type"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Type</FormLabel>
+                      <FormControl>
+                        <Input disabled placeholder="Type" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* <FormField
+                  control={form.control}
+                  name="value"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Value</FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select Value" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="C">C</SelectItem>
+                          <SelectItem value="E">E</SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                /> */}
+                {isLoading ? (
+                  <Button disabled>
+                    <Loader2 size={20} className="mr-2 animate-spin" />
+                    <span>Updating Kpi...</span>
+                  </Button>
+                ) : (
+                  <Button type="submit">Submit</Button>
+                )}
+              </form>
+            </Form>
+            {/* <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter> */}
+          </ScrollArea>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default Update;
