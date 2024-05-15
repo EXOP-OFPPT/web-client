348b1f6acd08c53012f673adee61ba32527a4f04
KAMADO <abdonsila2004@gmail.com>
2024-05-15 23:05:49 +0100

    refactor: Update ResetPassword component to fix import path and add arrow image for navigation

--- src/components/Auth/ResetPassword.tsx
+++ src/components/Auth/ResetPassword.tsx
@@ -15,7 +15,7 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/state/store";
 import { useToast } from "../ui/use-toast";
-import { clearMessageAndError, resetPassword } from "@/state/auth/AuthSlice";
+import { clearMessageAndError, resetPassword } from "@/state/Auth/AuthSlice";
 import { CheckCircle2, Loader2 } from "lucide-react";
 import { Input } from "../ui/input";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -23,6 +23,7 @@ import { Toaster } from "../ui/toaster";
 import { Button } from "../ui/button";
 import { Card } from "../ui/card";
 import { ModeToggle } from "../global/mode-toggle";
+import arrow from "@/assets/arrow.svg";
 
 const formSchema = z.object({
   email: z.string().email({
@@ -42,7 +43,7 @@ const ResetPassword: React.FC = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      email: "abdonsila222@gmail.com",
+      email: "",
     },
   });
 
@@ -86,6 +87,7 @@ const ResetPassword: React.FC = () => {
             className="w-[90%] md:w-[550px] h-80"
             onSubmit={form.handleSubmit(onSubmit)}
           >
+            <img onClick={() => navigate("/login")} className="relative top-[60%] right-5 cursor-pointer bg-primary rounded-full p-2" src={arrow} />
             <Card className="w-full h-full rounded-3xl flex flex-col gap-4 justify-center items-center px-10">
               <div className="social-icons">
                 <img
