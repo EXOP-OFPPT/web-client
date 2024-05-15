25dcd986d0ac383b0330332b89015a75056a5d13
KAMADO <abdonsila2004@gmail.com>
2024-05-15 23:08:28 +0100

    refactor: Add DisplayPostsCards component for displaying post cards

---
+++ src/components/Posts/Display.tsx
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux";
+import { RootState } from "@/state/store";
+import PostCard from "./PostCard";
+import { Card } from "../ui/card";
+
+export default function DisplayPostsCards() {
+  const data = useSelector((state: RootState) => state.getPosts.posts);
+
+  return (
+    <div className="w-full h-auto flex justify-center pb-10 mt-24">
+      <Card className="w-auto max-w-[900px] bg-transparent prr-16">
+        {data.map((item, index) => {
+          return <PostCard key={index} data={item} />;
+        })}
+        {/* {data.map((item, index) => {
+          return <PostCard key={index} data={item} />;
+        })} */}
+      </Card>
+    </div>
+  );
+}
