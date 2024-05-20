973c025f0cda32338f6f937dfe0fed096a5cc3d3
KAMADO <abdonsila2004@gmail.com>
2024-05-20 01:45:57 +0100

    refactor: Update import paths in DisplayPostsCards component

--- src/components/Posts/Display.tsx
+++ src/components/Posts/Display.tsx
@@ -2,14 +2,15 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/state/store";
 import PostCard from "./PostCard";
 import { Card } from "../ui/card";
+import { PostType } from "@/state/Posts/GetSlice";
 
 export default function DisplayPostsCards() {
   const data = useSelector((state: RootState) => state.getPosts.posts);
 
   return (
-    <div className="w-full h-auto flex justify-center pb-10 mt-24">
-      <Card className="w-auto max-w-[900px] bg-transparent prr-16">
-        {data.map((item:any, index:any) => {
+    <div className="w-full h-auto flex justify-center pb-10 mt-28">
+      <Card className="w-auto max-w-[900px] bg-transparent pr-5">
+        {data.map((item: PostType, index: number) => {
           return <PostCard key={index} data={item} />;
         })}
         {/* {data.map((item, index) => {
