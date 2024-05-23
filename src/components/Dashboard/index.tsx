08c10450fac6db6f6776c5e87f2de904828556d5
KAMADO <abdonsila2004@gmail.com>
2024-05-23 12:45:55 +0100

    refactor: Remove unused Dashboard components

--- src/components/Dashboard/index.tsx
+++
@@ -1,9 +0,0 @@
-// export { default as Button } from './Button';
-// export { default as ThemeSettings } from './ThemeSettings';
-// export { default as Sidebar } from './Sidebar';
-// // eslint-disable-next-line import/no-cycle
-// export { default as Navbar } from './Navbar';
-// export { default as Notification } from './Notification';
-// export { default as UserProfile } from './UserProfile';
-// export { default as Header } from './Header';
-
