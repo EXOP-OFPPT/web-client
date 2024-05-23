548c239328f454c66532132677ed7b304ad8e7bc
KAMADO <abdonsila2004@gmail.com>
2024-05-23 12:48:53 +0100

    refactor: Update Task columns styling and logic

--- src/components/Tasks/Create.tsx
+++ src/components/Tasks/Create.tsx
@@ -19,7 +19,6 @@ import { z } from "zod";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -55,11 +54,14 @@ import { useToast } from "../ui/use-toast";
 import { useEffect } from "react";
 import { Toaster } from "../ui/toaster";
 import { createTask, clearMessageAndError } from "@/state/Tasks/CreateSlice";
-import { Timestamp } from "firebase/firestore";
+import { doc, getDoc, Timestamp } from "firebase/firestore";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 import Cookies from "universal-cookie";
+import { useNavigate } from "react-router-dom";
+import { db } from "@/firebase/firebase";
 const cookies = new Cookies(null, { path: "/" });
+
 
 
 const formSchema = z.object({
@@ -74,9 +76,6 @@ const formSchema = z.object({
   }),
   status: z.string({
     required_error: "Status is required",
-  }),
-  bonus: z.string({
-    required_error: "Bonus Number is required",
   }),
   createdAt: z.string({
     required_error: "createdAt is required",
@@ -94,10 +93,9 @@ type CreateProps = {
   from: "global" | "kpi";
   mode: "ghost" | "outline";
   kpiCode: string;
-  availableBonus: number;
 };
 
-function AddTask({ from, mode, kpiCode, availableBonus }: CreateProps) {
+function AddTask({ from, mode, kpiCode }: CreateProps) {
   const user = cookies.get("user");
   const employees = useSelector((state: RootState) => state.getEmployees.employees);
   const isLoading = useSelector(
@@ -108,7 +106,9 @@ function AddTask({ from, mode, kpiCode, availableBonus }: CreateProps) {
   );
   const error = useSelector((state: RootState) => state.createTask.error);
   const [deadLine, setDeadLine] = React.useState<Date>();
+  const [kpiData, setKpiData] = React.useState<any>();
   const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
   const { toast } = useToast();
 
 
@@ -120,6 +120,7 @@ function AddTask({ from, mode, kpiCode, availableBonus }: CreateProps) {
 
   // Then, in your component
   useEffect(() => {
+    getDynamicKpiData();
     if (message) {
       toast({
         variant: "default",
@@ -139,7 +140,23 @@ function AddTask({ from, mode, kpiCode, availableBonus }: CreateProps) {
       });
       dispatch(clearMessageAndError());
     }
-  }, [message, error]);
+  }, [message, error, dispatch]);
+
+
+  const getDynamicKpiData = async () => {
+    try {
+      const docRef = doc(db, "kpis", kpiCode);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        const data = docSnap.data();
+        setKpiData(data);
+      } else {
+        navigate("/app/kpis");
+      }
+    } catch (error: any) {
+      navigate("/app/kpis");
+    }
+  };
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
@@ -149,7 +166,6 @@ function AddTask({ from, mode, kpiCode, availableBonus }: CreateProps) {
       title: "Task 5",
       description: "Description Task 5",
       status: "todo",
-      bonus: "0",
       createdAt: format(new Date(), 'yyyy-MM-dd'),
       assignedTo: "",
       kpiCode: kpiCode,
@@ -167,211 +183,194 @@ function AddTask({ from, mode, kpiCode, availableBonus }: CreateProps) {
       title: values.title,
       description: values.description,
       status: values.status,
-      bonus: parseInt(values.bonus),
       createdAt: createdAtTimestamp,
       deadLine: deadLineTimestamp,
       assignedTo: values.assignedTo,
       kpiCode: values.kpiCode,
     };
-    dispatch(createTask({ docId: docId, taskData: data, user: { role: user.role, email: user.email } }));
+    // Get current Component url
+    const url = window.location.pathname;
+    const from = url.substring(url.lastIndexOf('/') + 1);
+    dispatch(createTask({ docId: docId, taskData: data, from: from, email: user.email }));
   }
-
-  return (
-    <div className={availableBonus == 0 ? "bg-secondary pointer-events-none opacity-50" : ""}>
-      <Toaster />
-      <Dialog>
-        <DialogTrigger asChild>
-          {from == "kpi" ?
-            <Button>
-              <ListPlusIcon size={20} className="mr-2" />
-              <span>Add Task</span>
-            </Button>
-            :
-            <Button
-              className="h-8 w-8 hover:text-primary"
-              variant={mode}
-              size="icon"
-            >
-              <ListPlusIcon size={16} />
-            </Button>
-          }
-        </DialogTrigger>
-        <DialogContent className="h-[650px] sm:max-w-[700px] px-1">
-          <ScrollArea className="h-full w-full p-4">
-            <DialogHeader className="mb-4">
-              <DialogTitle className="text-primary text-2xl">
-                Create Task
-              </DialogTitle>
-              <DialogDescription>
-                Fill in the form below to create a new task.
-              </DialogDescription>
-            </DialogHeader>
-            {/* <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4"> */}
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-8 px-2 my-2"
+  if (kpiData)
+    return (
+      <div className={kpiData.availableBonus == 0 || kpiData.currentTaux == 100 ? "bg-secondary pointer-events-none opacity-50" : ""}>
+        <Toaster />
+        <Dialog>
+          <DialogTrigger asChild>
+            {from == "kpi" ?
+              <Button>
+                <ListPlusIcon size={20} className="mr-2" />
+                <span>Add Task</span>
+              </Button>
+              :
+              <Button
+                className="h-8 w-8 hover:text-primary"
+                variant={mode}
+                size="icon"
               >
-                <FormField
-                  control={form.control}
-                  name="title"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Title</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Title" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="description"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Description</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Description" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="status"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Status</FormLabel>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
+                <ListPlusIcon size={16} />
+              </Button>
+            }
+          </DialogTrigger>
+          <DialogContent className="h-[650px] sm:max-w-[700px] px-1">
+            <ScrollArea className="h-full w-full p-4">
+              <DialogHeader className="mb-4">
+                <DialogTitle className="text-primary text-2xl">
+                  Create Task
+                </DialogTitle>
+                <DialogDescription>
+                  Fill in the form below to create a new task.
+                </DialogDescription>
+              </DialogHeader>
+              {/* <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4"> */}
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-8 px-2 my-2"
+                >
+                  <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Title</FormLabel>
                         <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select Status" />
-                          </SelectTrigger>
+                          <Input placeholder="Title" {...field} />
                         </FormControl>
-                        <SelectContent>
-                          <SelectItem value="todo">Todo</SelectItem>
-                          <SelectItem value="inprogress">In Progress</SelectItem>
-                          <SelectItem value="done">Done</SelectItem>
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="bonus"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Bonus</FormLabel>
-                      <FormControl>
-                        <Input min={1} max={availableBonus} type="number" placeholder="Bonus" {...field} />
-                      </FormControl>
-                      <FormDescription>
-                        Be careful this value can't be changed after
-                      </FormDescription>
-                      <FormDescription>
-                        Max Bonus to assigne to his task is {availableBonus}
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="createdAt"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Created At</FormLabel>
-                      <FormControl>
-                        <Input disabled type="date" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "w-full justify-start text-left font-normal",
-                        !deadLine && "text-muted-foreground"
-                      )}
-                    >
-                      <CalendarIcon className="mr-2 h-4 w-4" />
-                      {deadLine ? format(deadLine, "PPP") : <span>Pick a deadLine</span>}
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Description</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Description" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="status"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Status</FormLabel>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Select Status" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="todo">Todo</SelectItem>
+                            <SelectItem value="inprogress">In Progress</SelectItem>
+                            <SelectItem value="done">Done</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="createdAt"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Created At</FormLabel>
+                        <FormControl>
+                          <Input disabled type="date" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-full justify-start text-left font-normal",
+                          !deadLine && "text-muted-foreground"
+                        )}
+                      >
+                        <CalendarIcon className="mr-2 h-4 w-4" />
+                        {deadLine ? format(deadLine, "PPP") : <span>Pick a deadLine</span>}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0">
+                      <Calendar
+                        mode="single"
+                        selected={deadLine}
+                        onSelect={setDeadLine}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormField
+                    control={form.control}
+                    name="assignedTo"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Assigned To</FormLabel>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Assigned To" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            {employees.map((employee) => (
+                              <SelectItem key={employee.email} value={employee.email}>
+                                <div className="w-full h-full flex justify-center items-center gap-2">
+                                  <Avatar className="w-6 h-6 my-2 flex items-center justify-center cursor-pointer">
+                                    <AvatarImage loading="lazy" src={employee.avatar} className="object-cover" />
+                                    <AvatarFallback className="text-[9px]">
+                                      {employee.firstName?.charAt(0).toUpperCase()}
+                                      {employee.lastName?.charAt(0).toUpperCase()}
+                                    </AvatarFallback>
+                                  </Avatar>
+                                  <span>{employee.email}</span>
+                                </div>
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {isLoading ? (
+                    <Button disabled>
+                      <Loader2 size={20} className="mr-2 animate-spin" />
+                      <span>Creating task...</span>
                     </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0">
-                    <Calendar
-                      mode="single"
-                      selected={deadLine}
-                      onSelect={setDeadLine}
-                      initialFocus
-                    />
-                  </PopoverContent>
-                </Popover>
-                <FormField
-                  control={form.control}
-                  name="assignedTo"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Assigned To</FormLabel>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Assigned To" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          {employees.map((employee) => (
-                            <SelectItem key={employee.email} value={employee.email}>
-                              <div className="w-full h-full flex justify-center items-center gap-2">
-                                <Avatar className="w-6 h-6 my-2 flex items-center justify-center cursor-pointer">
-                                  <AvatarImage loading="lazy" src={employee.avatar} className="object-cover" />
-                                  <AvatarFallback className="text-[10px]">
-                                    {employee.firstName?.charAt(0).toUpperCase()}
-                                    {employee.lastName?.charAt(0).toUpperCase()}
-                                  </AvatarFallback>
-                                </Avatar>
-                                <span>{employee.email}</span>
-                              </div>
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
+                  ) : (
+                    <Button type="submit">Submit</Button>
                   )}
-                />
-                {isLoading ? (
-                  <Button disabled>
-                    <Loader2 size={20} className="mr-2 animate-spin" />
-                    <span>Creating task...</span>
-                  </Button>
-                ) : (
-                  <Button type="submit">Submit</Button>
-                )}
-              </form>
-            </Form>
-            {/* <DialogFooter>
+                </form>
+              </Form>
+              {/* <DialogFooter>
           <Button type="submit">Save changes</Button>
         </DialogFooter> */}
-          </ScrollArea>
-        </DialogContent>
-      </Dialog>
-    </div>
-  );
+            </ScrollArea>
+          </DialogContent>
+        </Dialog>
+      </div>
+    );
 }
 
 export default AddTask;
