04cf817ada4f2f6fb9c7449a0517b4ee1591888a
KAMADO <abdonsila2004@gmail.com>
2024-06-01 19:18:18 +0100

    refactor: Optimize RecentInfo component to handle empty employee list

--- src/components/Dashboard/RecentInfo.tsx
+++ src/components/Dashboard/RecentInfo.tsx
@@ -6,14 +6,15 @@ export function RecentInfo() {
 
     const employees = useSelector((state: RootState) => state.getEmployees.employees)
 
-
-    return (
-        <div className="space-y-8">
-            {
-                employees.map((employee, index) =>
-                    <EmployeeProductivity key={index} employee={employee} />
-                )
-            }
-        </div>
-    );
+    if (!(employees.length === 0)) {
+        return (
+            <div className="space-y-8">
+                {
+                    employees.map((employee, index) =>
+                        <EmployeeProductivity key={index} employee={employee} />
+                    )
+                }
+            </div>
+        );
+    }
 }
