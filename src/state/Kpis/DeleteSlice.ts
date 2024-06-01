a95175a5abb133cc3e0c860b63c7f3bc6bf6bfbc
KAMADO <abdonsila2004@gmail.com>
2024-06-01 19:20:56 +0100

    refactor: Update DeleteSlice to delete contributions subcollection before deleting KPI

--- src/state/Kpis/DeleteSlice.ts
+++ src/state/Kpis/DeleteSlice.ts
@@ -1,13 +1,20 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { AppThunk } from "../store";
-import { deleteDoc, doc } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, query, where, writeBatch } from "firebase/firestore";
 import { db } from "@/firebase/firebase";
 import { getKpis } from "./GetSlice";
+import { updateContribution } from "../Employees/UpdateSlice";
 
 // Interface for error
 interface Error {
   code: string;
   message: string;
+}
+
+interface DeletePyload {
+  docId: string;
+  contribute: string;
+  email: string;
 }
 
 // Interface for DeleteState
@@ -64,16 +71,27 @@ export const {
 
 export default deleteKpiSlice.reducer;
 
-// Thunk to delete employee
+
+// Thunk to delete KPI
 export const deleteKpi =
-  (docId: string): AppThunk =>
+  ({ docId, contribute, email }: DeletePyload): AppThunk =>
     async (dispatch) => {
       // Reset message and error
       dispatch(setLoading(true));
       dispatch(clearMessageAndError());
       try {
-        // Delete employee document
+        // Delete tasks with kpiCode == docId
+        const tasksRef = collection(db, "tasks");
+        const q = query(tasksRef, where("kpiCode", "==", docId));
+        const querySnapshot = await getDocs(q);
+        const batch = writeBatch(db);
+        querySnapshot.docs.forEach((doc) => {
+          batch.delete(doc.ref);
+        });
+        await batch.commit();
+        // Delete KPI document
         deleteDoc(doc(db, "kpis", docId)).then(() => {
+          dispatch(updateContribution({ email, contribute }));
           dispatch(actionSuccess("Kpi deleted successfully"));
           dispatch(getKpis());
         }).catch((error) => {
@@ -81,5 +99,7 @@ export const deleteKpi =
         })
       } catch (error: any) {
         dispatch(actionFailed({ code: error.code, message: error.message }));
+      } finally {
+        dispatch(setLoading(false));
       }
     };
