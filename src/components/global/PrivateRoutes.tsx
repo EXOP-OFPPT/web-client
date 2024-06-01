4801f6107a20855cbc28a86e18c1ec83e64f7668
KAMADO <abdonsila2004@gmail.com>
2024-06-01 19:48:52 +0100

    refactor: Update PrivateRoutes.tsx to remove commented code and improve readability

--- src/components/global/PrivateRoutes.tsx
+++ src/components/global/PrivateRoutes.tsx
@@ -1,22 +1,22 @@
 import { Navigate } from "react-router-dom";
-// import { auth } from "@/firebase/firebase";
-// import { RootState } from "@/state/store";
-// import { useSelector } from "react-redux";
-// import Cookies from "universal-cookie";
-// const cookies = new Cookies(null, { path: "/" });
+import { auth } from "@/firebase/firebase";
+import { RootState } from "@/state/store";
+import { useSelector } from "react-redux";
+import Cookies from "universal-cookie";
+const cookies = new Cookies(null, { path: "/" });
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  // const user = cookies.get("user");
-  // const authUser = auth.currentUser;
-  // const reduxIsLoading = useSelector((state: RootState) => state.auth.isLoading);
+  const user = cookies.get("user");
+  const authUser = auth.currentUser;
+  const reduxIsLoading = useSelector((state: RootState) => state.auth.isLoading);
 
 
-  return true ? children : <Navigate to="/login" />;
-  // return (user && authUser) || reduxIsLoading ? children : <Navigate to="/login" />;
+  // return true ? children : <Navigate to="/login" />;
+  return (user && authUser) || reduxIsLoading ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
