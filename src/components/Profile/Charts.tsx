7b7923b1a6947670a70b4c8e6623058ef76f04e8
KAMADO <abdonsila2004@gmail.com>
2024-06-01 19:19:38 +0100

    refactor: Update Charts component to use cookies for user email and dispatch getEmployeeContributions

--- src/components/Profile/Charts.tsx
+++ src/components/Profile/Charts.tsx
@@ -1,17 +1,53 @@
-import React from "react";
-import { Card } from "../ui/card";
+import React, { useEffect } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import AreaChart from "../Dashboard/AreaChart";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/state/store";
+import Cookies from "universal-cookie";
+import { getEmployeeContributions } from "@/state/Employees/GetSlice";
+import { ThemeProviderContext } from "../global/theme-provider";
+const cookies = new Cookies(null, { path: "/" });
 
 type ChartsProps = {};
+type Theme = "dark" | "light" | "system"
+
 
 const Charts: React.FC<ChartsProps> = () => {
-  return (
-    <Card className="rounded-lg shadow-lg py-10">
-      <div className="font-bold pb-7 px-7 uppercase">
-        Vos statistique
-      </div>
-      <div className="flex flex-col items-end px-7"></div>
-    </Card>
-  );
+  const user = cookies.get("user");
+  const employeeContributes = useSelector((state: RootState) => state.getEmployees.employeeContributions)
+  const loading = useSelector((state: RootState) => state.getEmployees.loading)
+  const { theme } = React.useContext(ThemeProviderContext);
+  const [stateTheme, setStateTheme] = React.useState<"dark" | "light">("light");
+  const dispatch = useDispatch<AppDispatch>()
+
+  useEffect(() => {
+    dispatch(getEmployeeContributions(user.email))
+  }, [])
+
+  useEffect(() => {
+    let currentTheme: Theme = theme
+    if (currentTheme === "system") {
+      currentTheme = window.matchMedia("(prefers-color-scheme: dark)")
+        .matches
+        ? "dark"
+        : "light"
+    }
+    setStateTheme(currentTheme)
+  }, [theme])
+
+
+  if (!loading) {
+    return (
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle> Vos statistique</CardTitle>
+        </CardHeader>
+        <CardContent className="!p-0">
+          <AreaChart contributions={employeeContributes} theme={stateTheme} />
+        </CardContent>
+      </Card>
+    );
+  }
 };
 
 export default Charts;
