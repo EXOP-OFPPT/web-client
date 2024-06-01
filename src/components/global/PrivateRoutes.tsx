26582503f54dfda00584665cc40bbd06f6f5c6a1
KAMADO <abdonsila2004@gmail.com>
2024-06-01 19:25:14 +0100

    refactor: Remove commented code in PrivateRoutes.tsx

--- src/components/global/PrivateRoutes.tsx
+++ src/components/global/PrivateRoutes.tsx
@@ -1,18 +1,18 @@
-import { auth } from "@/firebase/firebase";
-import { RootState } from "@/state/store";
-import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-import Cookies from "universal-cookie";
-const cookies = new Cookies(null, { path: "/" });
+// import { auth } from "@/firebase/firebase";
+// import { RootState } from "@/state/store";
+// import { useSelector } from "react-redux";
+// import Cookies from "universal-cookie";
+// const cookies = new Cookies(null, { path: "/" });
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const user = cookies.get("user");
-  const authUser = auth.currentUser;
-  const reduxIsLoading = useSelector((state: RootState) => state.auth.isLoading);
+  // const user = cookies.get("user");
+  // const authUser = auth.currentUser;
+  // const reduxIsLoading = useSelector((state: RootState) => state.auth.isLoading);
 
 
   return true ? children : <Navigate to="/login" />;
