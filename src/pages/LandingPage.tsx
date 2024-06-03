eeb74d97ca389d407031814bc6d643e549cb81db
KAMADO <abdonsila2004@gmail.com>
2024-06-03 22:45:55 +0100

    feat: Add LandingPage component

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Landing Page!</h1>
+            <p>This is a basic landing page component.</p>
+        </div>
+    );
+};
+
+export default LandingPage;
