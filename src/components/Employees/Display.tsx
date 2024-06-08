a9fe8ece03c11bd34ae9c6d0ac1f60ff0b2d3eea
KAMADO <abdonsila2004@gmail.com>
2024-06-08 19:28:37 +0100

    refactor: Update DisplayEmployees component to use useSelector for user and data

--- src/components/Employees/Display.tsx
+++ src/components/Employees/Display.tsx
@@ -1,16 +1,15 @@
 import { useSelector } from "react-redux";
 import { RootState } from "@/state/store";
 import ProfileCard from "./ProfileCard";
-import Cookies from "universal-cookie";
-const cookies = new Cookies(null, { path: "/" });
+import { UserInterface } from "@/state/Auth/AuthSlice";
 
 export default function DisplayEmployees() {
-  const user = cookies.get("user");
+  const user = useSelector((state: RootState) => state.auth.user) as UserInterface;
   const data = useSelector((state: RootState) => state.getEmployees.employees);
 
   return (
     <div className="px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {data.filter(item => item.email !== user.email).map((item, index) => {
+      {data.filter(item => item.email !== user?.email).map((item, index) => {
         return <ProfileCard key={index} data={item} />;
       })}
     </div>
