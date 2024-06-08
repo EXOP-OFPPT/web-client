2f14a4aa2f8497da0bcb7257698f3cc0c1dc76d1
KAMADO <abdonsila2004@gmail.com>
2024-06-08 19:29:32 +0100

    refactor: Remove unused code and optimize PrivateRoutes component

--- src/components/global/PrivateRoutes.tsx
+++ src/components/global/PrivateRoutes.tsx
@@ -2,21 +2,19 @@ import { Navigate } from "react-router-dom";
 import { auth } from "@/firebase/firebase";
 import { RootState } from "@/state/store";
 import { useSelector } from "react-redux";
-import Cookies from "universal-cookie";
-const cookies = new Cookies(null, { path: "/" });
+
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const user = cookies.get("user");
   const authUser = auth.currentUser;
-  const reduxIsLoading = useSelector((state: RootState) => state.auth.isLoading);
+  const user = useSelector((state: RootState) => state.auth.user);
 
 
   // return true ? children : <Navigate to="/login" />;
-  return (user && authUser) || reduxIsLoading ? children : <Navigate to="/login" />;
+  return (user && authUser) ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
