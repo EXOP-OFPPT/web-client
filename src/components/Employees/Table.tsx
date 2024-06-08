fcc47d47a9da1aa6857de0b994e3e60c23430f20
KAMADO <abdonsila2004@gmail.com>
2024-06-08 19:28:44 +0100

    refactor: Update EmployeesTable component to use useSelector for user and data

--- src/components/Employees/Table.tsx
+++ src/components/Employees/Table.tsx
@@ -2,18 +2,17 @@ import { useSelector } from "react-redux";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
 import { RootState } from "@/state/store";
-import Cookies from "universal-cookie";
-const cookies = new Cookies(null, { path: "/" });
+import { UserInterface } from "@/state/Auth/AuthSlice";
 
 export default function EmployeesTable() {
-  const user = cookies.get("user");
+  const user = useSelector((state: RootState) => state.auth.user) as UserInterface;
   const data = useSelector((state: RootState) => state.getEmployees.employees);
 
 
 
   return (
     <div className="w-full xlg:w-4/5 md:w-11/12">
-      <DataTable columns={columns} data={data.filter(item => item.email !== user.email)} />
+      <DataTable columns={columns} data={data.filter(item => item.email !== user?.email)} />
     </div>
   );
 }
