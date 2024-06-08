373484e881d4f6852b2e9b60f408a34dbd1d7b65
KAMADO <abdonsila2004@gmail.com>
2024-06-08 19:35:15 +0100

    refactor: Update Charts component to use Redux store for user authentication

--- src/components/Profile/Charts.tsx
+++ src/components/Profile/Charts.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
-import Cookies from "universal-cookie";
 import { ThemeProviderContext } from "../global/theme-provider";
 import { EmployeeType } from "@/state/Employees/GetSlice";
 import AreaChartComponent from "../Charts/AreaChartComponent";
-const cookies = new Cookies(null, { path: "/" });
+import { useSelector } from "react-redux";
+import { RootState } from "@/state/store";
+import { UserInterface } from "@/state/Auth/AuthSlice";
 
 type ChartsProps = {};
 type Theme = "dark" | "light" | "system"
 
 
 const Charts: React.FC<ChartsProps> = () => {
-  const user = cookies.get("user");
+  const user = useSelector((state: RootState) => state.auth.user) as UserInterface;
   const { theme } = React.useContext(ThemeProviderContext);
   const [stateTheme, setStateTheme] = React.useState<"dark" | "light">("light");
 
@@ -30,7 +31,7 @@ const Charts: React.FC<ChartsProps> = () => {
   return (
     <Card className="w-full">
       <CardHeader>
-        <CardTitle> Vos statistique</CardTitle>
+        <CardTitle> Your statistique</CardTitle>
       </CardHeader>
       <CardContent className="!p-0">
         <AreaChartComponent employee={user as EmployeeType} theme={stateTheme} />
